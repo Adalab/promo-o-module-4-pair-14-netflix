ca3902fa665cdbe77d5ae9c52870b4824ab341dc
Laura Carbajales <laura.carbajales@gmail.com>
2022-02-03 20:00:11 +0100

    Update (day 3): Static server for images, 'POST' request for user login, and keep the user logged when refreshing the page.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const movies = require('./movies.json');
+const movies = require('./data/movies.json');
+const users = require('./data/users.json');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+//Servidor de estaticos
+const staticServerPath = './src/public-react';
+server.use(express.static(staticServerPath));
+//Servidor de estaticos imagenes
+const staticServerImage = './src/public-movies-images';
+server.use(express.static(staticServerImage));
 
 // init express aplication
 const serverPort = 4000;
@@ -38,4 +46,24 @@ server.get('/movies', (req, res) => {
 
   response.movies = filteredData;
   res.json(response);
+});
+
+server.post('/login', (req, res) => {
+  console.log('Petición a la ruta POST /login');
+  const emailLogin = req.body.email;
+  const passwordLogin = req.body.password;
+  const foundUser = users.find(
+    (eachUser) => eachUser.password === passwordLogin && eachUser.email === emailLogin
+  );
+  if (foundUser) {
+    res.json({
+      success: true,
+      userId: foundUser.id,
+    });
+  } else {
+    res.json({
+      success: false,
+      errorMessage: 'Usuaria/o no encontrada/o',
+    });
+  }
 });
