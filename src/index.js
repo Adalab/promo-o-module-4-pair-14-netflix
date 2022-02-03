78a1328cc4c42659885849840adf0b0e469bff29
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2022-02-03 11:29:18 +0100

    update day 3

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+//servidor de estáticos 
+const staticServerPath = "./src/public";
+server.use(express.static(staticServerPath));
+
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
@@ -15,8 +19,22 @@ server.listen(serverPort, () => {
 
 server.get('/movies', (req, res) => {
   console.log('Petición a la ruta GET / ');
-  const response = movies;
-  //Me devuelve error 500
-  //const filteredData = response.filter((movie) => movie.gender === genderFilterParam);
+  const response = {
+    success:true,
+    movies:[]
+  };
+  const genderFilterParam=req.query.gender;
+  const sortFilterParam = req.query.sort;
+  const filteredData = movies.movies.movies.filter((movie) => {
+    if(genderFilterParam){
+      return movie.gender===genderFilterParam;
+    }
+    return true;
+  });
+  response.movies=filteredData;
   res.json(response);
 });
+
+//servidor de estáticos imagenes
+const staticServerImages = "./src/public-movies-images";
+server.use(express.static(staticServerImages));
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,12 +1,12 @@
 // login
 
-const getMoviesFromApi = (gender,sort) => {
+const getMoviesFromApi = (params) => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch(`http://localhost:4000/movies?gender=${gender}&sort=${sort}`, { method: 'GET' })
+  return fetch(`http://localhost:4000/movies?gender=${params.gender}&sort=${params.sort}`, { method: 'GET' })
     .then((response) => response.json())
     .then((data) => {
-      return data.movies.movies;
+      return data.movies;
     });
 };
 
